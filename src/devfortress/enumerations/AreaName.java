8c9a33cfcf8602c2b1a96def89cac0ae069ba10a
Vinh Thanh <s3245715@rmit.edu.vn>
2012-11-03 22:43:49 +0700

    update

--- src/devfortress/enumerations/AreaName.java
+++ src/devfortress/enumerations/AreaName.java
@@ -12,7 +12,8 @@ public enum AreaName {
 
     //Area Name
     CODING("Coding"),
-    PROJECT_MANAGING("Project Managing"),
+    SECURITY("Security"),
+    PROJECT_MANAGEMENT("Project Management"),
     DATABASE_DESIGN("Database Designing"),
     SYSTEM_DESIGN("System Designing"),
     SQL_DESIGN("SQL Designing"),
@@ -25,14 +26,14 @@ public enum AreaName {
     ITERATION_TESTING("Iteration Testing"),
     SYSTEM_TESTING("System Testing"),
     TEST_MANAGEMENT("Test Management"),
-    SYSTEM_ANALYZE("System Analyze"),
-    UI_ANALYZE("UI Analyze"),
+    SYSTEM_ANALYZING("System Analyzing"),
+    UI_ANALYZING("UI Analyzing"),
     UI_DEVELOPMENT("UI Development"),
     DOCUMENTATION("Documentation"),
     DATABASE_DEVELOPMENT("Database Development"),
     NON_FUCNTIONAL_DEVELOPMENT("Non-Functional Development"),
     SQL_DEVELOPMENT("SQL_Development"),
-    FUCNTIONAL_DEVELOPMENT("Non-Functional Development"),
+    FUCNTIONAL_DEVELOPMENT("Functional Development"),
     RISK_MANAGEMENT("Risk Management"),
     PROBLEM_SOLVING("Problem Solving"),
     CONFIG_MANAGEMENT("Config Manage"),
@@ -41,7 +42,8 @@ public enum AreaName {
     WEB_DESIGN("Web Design"),
     WEB_DEVELOPMENT("Web Development"),
     SERVER_MANAGEMENT("Server Management"),
-    TOOLS_MANAGEMENT("Tools Management");
+    PROCESS_MANAGEMENT("Process Management"),
+    TOOL_MANAGEMENT("Tool Management");
     private String name;
 
     private AreaName(String name) {
