5c61181d06f685c162858f70f99728930b9e42b3
Quan To <btquanto@gmail.com>
2012-11-13 10:15:07 +0700

    restore

---
+++ src/devfortress/view/Colour.java
@@ -0,0 +1,48 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package devfortress.view;
+
+import java.awt.Color;
+
+/**
+ *
+ * @author PC
+ */
+public class Colour {
+
+    public static Color matenga = new Color(184, 250, 228);
+    public static Color orange = new Color(252,183,48);
+    public static Color lightGreen = new Color(222, 252, 223);
+    public static Color darkGreen = new Color(44, 148, 12);
+    public static Color lightGreenYellow = new Color(226, 248, 150);
+    public static Color darkGreenYellow = new Color(109, 139, 4);
+    public static Color lovelyPink = new Color(255, 175, 211);
+    public static Color youngGreen = new Color(138, 243, 141);
+    public static Color darkBlue = new Color(5, 49, 99);
+    public static Color darkBlue2 = new Color(36, 75, 146);
+    public static Color middleBlue = new Color(4, 101, 201);
+    public static Color darkGrey = new Color(58, 70, 93);
+    public static Color greenBlue = new Color(162, 250, 214);
+    public static Color purple = new Color(236, 188, 252);
+    public static Color yellow = new Color(251, 253, 124);
+    public static Color darkPink = new Color(248, 82, 121);
+    public static Color lightRed = new Color(253, 230, 230);
+    public static Color middleRed = new Color(254,104,118);
+    public static Color lightBlue1 = new Color(228, 243, 254);
+    public static Color lightBlue2 = new Color(141, 202, 253);
+    public static Color lightBlue3 = new Color(74, 191, 252);
+    public static Color lightBlue4 = new Color(51, 183, 253);
+    public static Color lightBlue5 = new Color(26, 173, 251);
+    public static Color lightBlue6 = new Color(6, 168, 253);
+    public static Color redOrange = new Color(251,100,75);
+    public static Color redOrangeDark = new Color(251,88,37);
+    public static Color blueGrey = new Color(99,120,187);
+    public static Color lightOrange = new Color(253,246,213);
+    public static Color brown = new Color(62,17,4);
+    public static Color darkOrange = new Color(234,178,44);
+    public static Color greenYellow = new Color(169,234,44);
+    public static Color greenYellow2 = new Color(144,215,8);
+    public static Color deepBlue = new Color(8,81,215);
+}
