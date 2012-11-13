141179308a1cbb282ceda68f9fb3e63122cf0a16
Quan To <btquanto@gmail.com>
2012-11-13 09:03:14 +0700

    resolve conflict

--- src/devfortress/DevFortress.java
+++ src/devfortress/DevFortress.java
@@ -1,19 +1,6 @@
 package devfortress;
 
 import devfortress.utilities.Utilities;
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.PrintWriter;
-import java.nio.charset.Charset;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Set;
 
 /**
  *
@@ -25,13 +12,10 @@ public class DevFortress {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        List<String> names = null;
         long t1 = System.currentTimeMillis();
-        for (long i = 0; i < 1000000000; i++) {
-            names = Utilities.getNameList();
+        for (int i = 0; i < 1000000000; i++) {
+            Utilities.getNameList();
         }
-        System.out.println("Done: " + (System.currentTimeMillis() - t1));
-        System.out.println(names.size());
-
+        System.out.println(System.currentTimeMillis() - t1);
     }
 }
