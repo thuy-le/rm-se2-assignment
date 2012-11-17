c75a6077327baf3c57d12d0efac9bfe3bf5b3212
Quan To <btquanto@gmail.com>
2012-11-17 12:00:35 +0700

    Please enter the commit message for your changes. Lines starting

---
+++ src/devfortress/controllers/Initilizable.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package devfortress.controllers;
+
+/**
+ *
+ * @author Team Poseidon
+ */
+public interface Initilizable {
+
+    void initilize();
+}
