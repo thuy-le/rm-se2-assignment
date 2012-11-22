109a2ca6868d971a667da6bef55ad18f9ac5bc44
Quan To <btquanto@gmail.com>
2012-11-22 14:21:03 +0700

    improve TrainSkillDialog

--- src/devfortress/view/TrainSkillDialog.java
+++ src/devfortress/view/TrainSkillDialog.java
@@ -32,7 +32,7 @@ public class TrainSkillDialog extends JFrame implements Observer {
         setSize(800, 600);
         setLayout(new BorderLayout());
         developer = new Developer();
-        skillModel = new DefaultTableModel(0, 2);
+        skillModel = new DefaultTableModel(0, 3);
         skillTable = new CustomTable(skillModel);
         infos = SkillInfo.values();
         Object[][] objs = new Object[infos.length][2];
@@ -42,26 +42,27 @@ public class TrainSkillDialog extends JFrame implements Observer {
                 String name = infos[i].getName();
                 Skill skill = developer.getSkills().get(infos[i]);
                 if (skill == null) {
-                    Object[] os = {infos[i].getName(), 0, new CustomButton("Train")};
+                    Object[] os = {infos[i].getType(), infos[i].getName(), 0};
                     skillModel.addRow(os);
                 } else {
-                    Object[] os = {infos[i].getName(), skill.getLevel(), new CustomButton("Train")};
+                    Object[] os = {infos[i].getType(), infos[i].getName(), skill.getLevel()};
                     skillModel.addRow(os);
                 }
-
-
             } catch (Exception ex) {
             }
         }
-        Object[] ids = {"Skill Name", "Level"};
+        Object[] ids = {"Type", "Skill Name", "Level"};
         skillModel.setColumnIdentifiers(ids);
         skillTable.setAutoResizeMode(JTable.AUTO_RESIZE_SUBSEQUENT_COLUMNS);
-        skillTable.getColumnModel().getColumn(1).setMaxWidth(80);
+        skillTable.getColumnModel().getColumn(0).setMaxWidth(200);
+        skillTable.getColumnModel().getColumn(0).setMinWidth(200);
+        skillTable.getColumnModel().getColumn(2).setMaxWidth(80);
         add(((CustomTable) skillTable).getTableScroll(), BorderLayout.CENTER);
         SelectionListener selectionListener = new SelectionListener(skillTable);
         skillTable.getSelectionModel().addListSelectionListener(selectionListener);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setTitle(developer.getName() + " - Training");
     }
 
     public TrainSkillDialog(Developer developer) {
@@ -85,17 +86,21 @@ public class TrainSkillDialog extends JFrame implements Observer {
 
     private class SelectionListener implements ListSelectionListener {
 
-        JTable table;
+        private JTable table;
+        private int selectedIndex;
 
         SelectionListener(JTable table) {
             this.table = table;
+            selectedIndex = -1;
         }
 
         @Override
         public void valueChanged(ListSelectionEvent e) {
-            if (e.getSource() == table.getSelectionModel() && table.getRowSelectionAllowed()) {
-                int current = table.getSelectedRow();
+            int current = table.getSelectedRow();
+            if (selectedIndex != current && e.getSource() == table.getSelectionModel() && table.getRowSelectionAllowed()) {
                 System.out.println(infos[current]);
+                System.out.println("---------");
+                selectedIndex = current;
             } else if (e.getSource() == table.getColumnModel().getSelectionModel() && table.getColumnSelectionAllowed()) {
                 int first = e.getFirstIndex();
                 int last = e.getLastIndex();
