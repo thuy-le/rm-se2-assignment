7503dfc3a2bd9376df350d3d0939feb1c80bbca9
Quan To <btquanto@gmail.com>
2012-12-20 20:15:48 +0700

    add setting dialog for automation

---
+++ src/devfortress/view/SettingDialog.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package devfortress.view;
+
+/**
+ *
+ * @author Team Poseidon
+ */
+public class SettingDialog {
+    
+}
